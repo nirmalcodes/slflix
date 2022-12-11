516622e64a96ec059ed9bb04b12fc34ed8645796
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2022-12-12 01:02:33 +0530

    Initiate new slflix v1.5
    the new slflix v1.5 is old v1.0s redesign with error correction

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="bg-slate-800">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div>App</div>
     </div>
   );
 }
